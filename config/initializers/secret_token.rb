6a03879b5ca0ec10f6b56864e90d2ef6529dbcc0
Dean Dieker <ddieker@gmail.com>
2012-06-20 21:58:29 -0400

    chapter 3 hartl setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'daacd86576a71a97f8f409677801478485bde3f80fea775358494b66aed4688679984c03eb91959269e185eadc1b3c237d0af70f042603d7e0cf044fb3c6f4d9'
